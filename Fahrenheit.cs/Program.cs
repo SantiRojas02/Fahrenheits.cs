70cd83fe70c8b8996c747c63c5671545df4551eb
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-08 21:30:44 -0300

    GradosFahrenheit

---
+++ Fahrenheit.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fahrenheit.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese grado celsius: ");
+
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fahrenheit: " + Convert.ToDecimal(celsius * 1.8 + 32));
+
+            Console.ReadKey();
+        }
+    }
+}
